0d0466ace03a2cec489d81b86c58e9c37faf85d8
Jeph Bayfield <jeph_b@hotmail.com>
2022-01-06 00:28:26 +0000

    Commenting for final 2 top level sprocs

--- src/DrSproc.App/Base/ITargetDatabase.cs
+++ src/DrSproc.App/Base/ITargetDatabase.cs
@@ -2,11 +2,29 @@
 
 namespace DrSproc
 {
+    /// <summary>
+    /// <b>ITargetDatabase</b><br />
+    /// An Interface with options for creating a transaction for a Target Database.
+    /// </summary>
+    /// <typeparam name="TDatabase"></typeparam>
     public interface ITargetDatabase<TDatabase> : ITargetConnection
         where TDatabase : IDatabase, new()
     {
+        /// <summary>
+        /// <b>Begin Transaction</b> <br />
+        /// Creates a Transaction within a Target Database to link together multiple Stored Procedure calls
+        /// </summary>
+        /// <param name="isolationLevel">Sql Transaction Isolation Level</param>
+        /// <returns>ITargetTransaction - With options for executing Stored Procedures within the Target Transaction as well as performing any transactional commands <i>(commit or rollback)</i> to the transaction</returns>
         ITargetTransaction BeginTransaction(TransactionIsolation? isolationLevel = null);
 
+        /// <summary>
+        /// <b>Begin Transaction</b> <br />
+        /// Creates a Transaction within a Target Database to link together multiple Stored Procedure calls
+        /// </summary>
+        /// <param name="transaction">Output of Transaction for other calling Stored Procedures and performing transactional commands in other scopes</param>
+        /// <param name="isolationLevel">Sql Transaction Isolation Level</param>
+        /// <returns>ITargetTransaction - With options for executing Stored Procedures within the Target Transaction as well as performing any transactional commands <i>(commit or rollback)</i> to the transaction</returns>
         ITargetTransaction BeginTransaction(out ITransaction<TDatabase> transaction, TransactionIsolation? isolationLevel = null);
     }
 }
--- src/DrSproc.App/Base/ITargetTransaction.cs
+++ src/DrSproc.App/Base/ITargetTransaction.cs
@@ -3,14 +3,34 @@ using System.Threading.Tasks;
 
 namespace DrSproc
 {
+    /// <summary>
+    /// <b>ITargetTransaction</b> <br />
+    /// An Interface with options for performing transactional commands
+    /// </summary>
     public interface ITargetTransaction : ITargetConnection
     {
+        /// <summary>
+        /// <b>RollbackTransaction</b> <br />
+        /// Rolls back the target transaction reverting any changes within.
+        /// </summary>
         void RollbackTransaction();
 
+        /// <summary>
+        /// <b>RollbackTransactionAsync</b> <br />
+        /// Asynchronously rolls back the target transaction reverting any changes within.
+        /// </summary>
         Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// <b>CommitTransaction</b> <br />
+        /// Commits the target transaction confirming any changes within.
+        /// </summary>
         void CommitTransaction();
 
+        /// <summary>
+        /// <b>CommitTransactionAsync</b> <br />
+        /// Asynchronously commits the target transaction confirming any changes within.
+        /// </summary>
         Task CommitTransactionAsync(CancellationToken cancellationToken = default);
     }
 }
