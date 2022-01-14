b3566d4556bc784e49ee5fd2c89fe95a6dc89226
Jeph Bayfield <jeph_b@hotmail.com>
2022-01-14 01:37:14 +0000

    documentation

--- src/DrSproc.DependencyInjection/DependencyInjectionExtensions.cs
+++ src/DrSproc.DependencyInjection/DependencyInjectionExtensions.cs
@@ -5,8 +5,14 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace DrSproc.Registration
 {
+    /// <summary>
+    /// Dependency Injection Extensions for Dr Sproc
+    /// </summary>
     public static class DependencyInjectionExtensions
     {
+        /// <summary>
+        /// Register Dr Sproc in your Startup class
+        /// </summary>
         public static void RegisterDrSproc(this IServiceCollection services)
         {
             services.AddScoped<ISqlConnector, SqlConnector>();
